44d2482c5f19b9532b6b7a7f61bdd22839ccf54b
ryouske1213 <san257718@yahoo.com.tw>
2022-12-05 15:31:51 +0800

    資料連動調整OK

--- components/pc-card.vue
+++ components/pc-card.vue
@@ -2,26 +2,27 @@
   <div class="pc-card">
     <div class="w-full min-h-screen flex justify-center items-center">
       <div class="w-[300px] h-[300px] bg-slate-500 opacity-70">
-        <select>
-          <option>選擇地區:</option>
+        <select v-model="citySelect">
+          <option value="null">選擇地區:</option>
+          <option v-for="(city, index) in citys" :key="index" :value="index">
+            {{ city.city }}
+          </option>
         </select>
-        <div v-for="link in mountains.records.location" :key="link.title">
+        <div v-if="districts.weatherElement !== undefined">
           <div class="flex justify-center text-lg">
-            <h1>天氣: {{link.weatherElement[0].time[0].parameter.parameterName}}</h1>
-            <!-- <p>{{ link }}</p> -->
+            <h1>天氣: {{ districts.weatherElement[0].time[0].parameter.parameterName }}</h1>
             <fa-icon icon="sun" />
           </div>
           <div>
-            <span>地區: {{link.locationName}}</span>
+            <span>地區: {{ districts.locationName }}</span>
           </div>
           <div>
-            <span>開始時間: {{link.weatherElement[0].time[0].startTime}}</span>
+            <span>開始時間: {{ districts.weatherElement[0].time[0].startTime }}</span>
           </div>
           <div>
-            <span>結束時間: {{link.weatherElement[0].time[0].endTime}}</span>
+            <span>結束時間: {{ districts.weatherElement[0].time[0].endTime }}</span>
           </div>
         </div>
-        <div>{{returnMountains}}</div>
       </div>
     </div>
   </div>
@@ -30,20 +31,45 @@
 export default {
   name: "PCcard",
 
+  data() {
+    return {
+      citySelect: null,
+      citys: [
+        {
+          city: "嘉義縣",
+        },
+        {
+          city: "新北市",
+        },
+        {
+          city: "嘉義市",
+        },
+        {
+          city: "新竹縣",
+        },
+        {
+          city: "新竹市",
+        },
+      ],
+    };
+  },
+
   props: {
     mountains: {
       type: Object,
-      required: () => {}
-    }
+      required: () => {},
+    },
   },
 
   computed: {
-    returnMountains(){
-      if(this.mountains) {
-        console.log(this.mountains);
+    districts() {
+      let districts = [];
+      if (this.citySelect !== null) {
+        // districts = this.citys[this.citySelect];
+        districts = this.mountains.records.location[this.citySelect];
       }
-      return ''
-    }
-  }
+      return districts;
+    },
+  },
 };
 </script>
